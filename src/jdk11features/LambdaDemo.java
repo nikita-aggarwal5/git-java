b8fd0facb3e19a09a94a7640aeb0461a2beb1a69
nikita-aggarwal5 <nikitaagarwal2906@gmail.com>
2022-09-05 23:34:42 +0530

    first commit

--- src/jdk11features/LambdaDemo.java
+++ src/jdk11features/LambdaDemo.java
@@ -1,19 +1,19 @@
 package jdk11features;
 
-//public class LambdaDemo {
-//    public static void main(String[] args){
-//        String name= "Hello";
-//        System.out.println(name.concat(" User"));
-//    }
-//}
-interface con{
-    public String say(String name);
-}
-public class LambdaDemo{
+public class LambdaDemo {
     public static void main(String[] args){
-        String con =(s1,s2)->s1+s2;
-        System.out.println(con("Hello","user"));
-
-
+        String name= "Hello";
+        System.out.println(name.concat(" User"));
     }
 }
+//interface con{
+//    public String say(String name);
+//}
+//public class LambdaDemo{
+//    public static void main(String[] args){
+//        String con =(s1,s2)->s1+s2;
+//        System.out.println(con("Hello","user"));
+//
+//
+//    }
+//}
