f477abc20dee1efbfc622d4c19957eaae910aae2
nikita-aggarwal5 <nikitaagarwal2906@gmail.com>
2022-09-05 14:11:46 +0530

    message

---
+++ src/Stringdemos/StringExample.java
@@ -0,0 +1,10 @@
+package Stringdemos;
+
+public class StringExample {
+    public static void main (String[] args){
+//        String obj1=new String(original: "Hello");
+
+
+
+    }
+}
---
+++ src/additiontwonum.java
@@ -0,0 +1,7 @@
+public class additiontwonum {
+    public static void main(String[]args){
+        int num1=15,num2=5,sum;
+        sum=num1+num2;
+        System.out.println("sum of these numbers: is:"+sum);
+    }
+}
